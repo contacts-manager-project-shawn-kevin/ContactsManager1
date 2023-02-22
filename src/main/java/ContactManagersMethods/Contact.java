5621570045d7d4c390d06eb71a724371e8bfe1a3
shawn hardin <shawn.a.hardin01@gmail.com>
2023-02-22 12:27:42 -0600

    save

--- src/main/java/ContactManagersMethods/Contact.java
+++ src/main/java/ContactManagersMethods/Contact.java
@@ -1,6 +1,31 @@
 package ContactManagersMethods;
 
 public class Contact {
+    private String name;
+     private String number;
+    public String getName() {
+        System.out.print(name + "\n");
+        return "";
+    }
+
+    public String getNumber() {
+        System.out.println(String.valueOf(number).replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1)-$2-$3"));
+        return "";
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Contact moe = new Contact();
+        moe.name = "Moe";
+        moe.number = String.valueOf(2103674647);
+        moe.getName();
+        moe.getNumber();
+
+
+    }
 
 
 
